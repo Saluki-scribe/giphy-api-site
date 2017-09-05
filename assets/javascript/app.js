c6ca7c8cd824a866b8b68a247a536b3aa015b086
saluki-scribe <tara@nelsonwriting.com>
2017-09-04 21:44:30 -0400

    Added if statement to stop and start gif animations.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    var shownGifs = ["Vampire", "Werewolf", "Zombie", "Witch", "Ghost", "Demon", "Hellhound", "Monster"];
+    var shownGifs = ["Vampire", "Werewolf", "Zombie", "Witch", "Ghost", "Demon", "Hellhound", "Monster", "Full Moon", "The Raven"];
     
 
 function searchGiphy() {
@@ -25,16 +25,61 @@ function searchGiphy() {
             var gifRating = response.data[i].rating;
             var gifImage = response.data[i].images.downsized.url;
             var gifStill = response.data[i].images.downsized_still.url;
-            
-            $("#gif-display").append("<div class = 'col-4'> <p> Rating: " + gifRating + "</p> <img src = '" + gifImage + "'></div>");
-        }
 
-        $("").on("click", function () {
-            if($(this).attr("data-state") == "still") {
-                $(this).attr("data-state") == "animated"
+            
+            
+            $("#gif-display").append("<div class = 'col-4 picture' > <p> Rating: " + gifRating + "</p> <img src = '" + gifStill + "' data-still ='" + gifStill + "' data-animate = '" + gifImage + "'></div>")    
+            console.log("Data-still: " + "data-still");
+
+           
+        } //End for Loop
+
+
+        $(".picture").on("click", function() {
+            var current = $(this).children("img").attr("src");
+            var animated = $(this).children("img").attr("data-animate");
+            var still = $(this).children("img").attr("data-still");
+            
+            console.log("current: " + current);
+            console.log("animated: " + animated);
+            console.log("still: " + still);
+            
+            
+            if (current == still) {
+                $(this).children("img").attr("src", animated);
+                console.log("last current: " + current);
             } else {
-                $(this).attr("data-state") == ""
+                $(this).children("img").attr("src", still);
             }
+            
+            
+                
+                //, "data-animate");
+                //console.log();
+                //.toggle(1000, function() {
+                   //$(this).attr("src", "data-animate");
+                //});    
+        });
+        
+
+        //    $(".picture").on("click", function () {
+        //        $(".picture").attr("src", gifImage);
+        //        console.log("This: " + $(".picture"));
+        //    })
+        
+       
+        
+
+        //$("img").on("click", function (event) {
+
+          //  $(this).html("<div class = 'col-4'> <p> Rating: " + gifRating + "</p> <img src = '" + gifImage + "'></div>");
+
+         //   console.log("This equals: " + this);
+            //if($(this).val() == gifStill) {
+            //    $(this) = gifImage;
+            //} else {
+            //    $(this).val() = gifStill;
+            //}
         
         });
 
@@ -44,7 +89,7 @@ function searchGiphy() {
        // $("#gif-display").append("<img src='" + response.data[1].images.downsized.url + "'>" );
         //$("#gif-display").append("<img src='" + response.data[2].images.downsized.url + "'>" );
         
-    })
+    //})
 } //End searchGiphy Function
 
 function renderButtons() {
